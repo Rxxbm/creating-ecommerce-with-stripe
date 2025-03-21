55b0ec246694442c81442aff875b5ae7df7109df
Rubem <rubemcorrea0@gmail.com>
2025-03-20 21:44:11 -0300

    fix: removing product_id from cart entity

--- src/entities/Cart/index.ts
+++ src/entities/Cart/index.ts
@@ -1,14 +1,9 @@
 import { Entity, Column, JoinColumn, ManyToOne } from "typeorm";
 import { BaseEntity } from "../BaseEntity";
-import { Product } from "../Product";
 import { Customer } from "../Customer";
 
 @Entity("cart")
 export class Cart extends BaseEntity {
-  @JoinColumn({ name: "product_id" })
-  @ManyToOne(() => Product)
-  product: Product;
-
   @JoinColumn({ name: "customer_id" })
   @ManyToOne(() => Customer)
   customer: Customer;
